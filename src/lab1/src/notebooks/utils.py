03edb907f84ad679411101c95ad8aa0a9ef56357
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-08 21:11:48 +0200

    Minor

--- src/lab1/src/notebooks/utils.py
+++ src/lab1/src/notebooks/utils.py
@@ -97,7 +97,7 @@ def numpy_matrix_to_network_graph(matrix: npmat.matrix) -> nx.Graph:
     """
     return nx.from_numpy_matrix(matrix)
 
-def print_matrix(matrix: npmat.matrix) -> None:
+def plot_graph(matrix: npmat.matrix) -> None:
     """
     Prints a matrix
 
