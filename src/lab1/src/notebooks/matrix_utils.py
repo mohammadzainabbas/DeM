ffc4446ca7020c1fbf3e1a1ea038dd211e3e2af7
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-08 20:57:33 +0200

    Minor update

--- src/lab1/src/notebooks/matrix_utils.py
+++ src/lab1/src/notebooks/matrix_utils.py
@@ -2,5 +2,14 @@ import numpy as np
 from numpy import matrixlib as npmat
 
 # ------------------------ #
-# Helper matrix functions
+# Helper binary relation
 # ------------------------ #
+def CompleteCheck(matrix: npmat.matrix) -> bool:
+    """
+    Checks if the matrix is complete
+    """
+    for i in range(n_row):
+        for j in range(n_col):
+            if matrix[i, j] == 0 or matrix[i, j] == 0:
+                return False
+    return True
