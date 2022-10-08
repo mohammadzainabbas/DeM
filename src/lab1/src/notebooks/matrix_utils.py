7346112d50f3a5482a5c1b629aba601f76324f1a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-08 20:57:54 +0200

    Work in progress

--- src/lab1/src/notebooks/matrix_utils.py
+++ src/lab1/src/notebooks/matrix_utils.py
@@ -8,6 +8,7 @@ def CompleteCheck(matrix: npmat.matrix) -> bool:
     """
     Checks if the matrix is complete
     """
+    
     for i in range(n_row):
         for j in range(n_col):
             if matrix[i, j] == 0 or matrix[i, j] == 0:
