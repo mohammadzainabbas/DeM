1be1341201084d9640de0b9fd6c43c687a03216f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-08 20:45:00 +0200

    Updated code

--- src/lab1/src/notebooks/utils.py
+++ src/lab1/src/notebooks/utils.py
@@ -1,4 +1,20 @@
 import sys
+
+# ------------------------ #
+# Helper logging functions
+# ------------------------ #
+def print_log(text: str) -> None:
+    """
+    Prints the log
+    """
+    print(f"[ log ]: {text}")
+
+def print_error(text: str) -> None:
+    """
+    Prints the error
+    """
+    print(f"[ error ]: {text}")
+
 if __name__ == "__main__":
     print("This is a module. Please import it.")
     sys.exit(1)
