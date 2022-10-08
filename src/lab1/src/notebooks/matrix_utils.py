5f4bd6b21d32dcf2b2a0deaedbbe8c030a7c7a59
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-08 21:00:20 +0200

    Updated code

--- src/lab1/src/notebooks/matrix_utils.py
+++ src/lab1/src/notebooks/matrix_utils.py
@@ -8,9 +8,19 @@ def CompleteCheck(matrix: npmat.matrix) -> bool:
     """
     Checks if the matrix is complete
     """
-    
+    n_row, n_col = matrix.shape
     for i in range(n_row):
         for j in range(n_col):
             if matrix[i, j] == 0 or matrix[i, j] == 0:
                 return False
+    return True
+
+def ReflexiveCheck(matrix: npmat.matrix) -> bool:
+    """
+    Checks if the matrix is reflexive
+    """
+    n_row, n_col = matrix.shape
+    for i in range(n_row):
+        if matrix[i, i] == 0:
+            return False
     return True
